1022d8c6ffeefb9e4ae89af5903df5e0b17ffc6d
Tauelangi Bryant <10865140@my.uvu.edu>
2020-09-01 16:34:16 -0600

    added MoverScript

---
+++ Cyborg Shrimp/Assets/Scripts/MoverScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float x, y, z;
+    public float speed = 3f;
+    void Start()
+    {
+        Debug.Log("Hello World");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = speed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        y = speed * Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.Translate(x, y, z);
+    }
+}
